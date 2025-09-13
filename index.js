8cfcbec8f709ba8a518b30eaf799bf4e387c8b1d
Omar Dukureh <132103818+oduku1@users.noreply.github.com>
2025-09-13 12:12:49 -0400

    new

--- index.js
+++ index.js
@@ -2,11 +2,22 @@
 // Select and cache the <main> element
 const mainEl = document.querySelector('main');
 const menuLinks = [
-    { text: 'about', href: '/about' },
-    { text: 'catalog', href: '/catalog' },
-    { text: 'orders', href: '/orders' },
-    { text: 'account', href: '/account' },
-  ];
+  {text: 'about', href: '/about'},
+  {text: 'catalog', href: '#', subLinks: [
+    {text: 'all', href: '/catalog/all'},
+    {text: 'top selling', href: '/catalog/top'},
+    {text: 'search', href: '/catalog/search'},
+  ]},
+  {text: 'orders', href: '#' , subLinks: [
+    {text: 'new', href: '/orders/new'},
+    {text: 'pending', href: '/orders/pending'},
+    {text: 'history', href: '/orders/history'},
+  ]},
+  {text: 'account', href: '#', subLinks: [
+    {text: 'profile', href: '/account/profile'},
+    {text: 'sign out', href: '/account/signout'},
+  ]},
+];
 
 // Set the background color using the CSS custom property
 mainEl.style.backgroundColor = 'var(--main-bg)';
@@ -31,3 +42,48 @@ menuLinks.forEach( menu => {
 });
 
 
+const subMenu = document.getElementById('sub-menu');
+subMenu.style.height = "100%";
+subMenu.style.background = "var(--sub-menu-bg)";
+subMenu.style.position = "absolute";  
+subMenu.style.top = "0%";
+subMenu.classList.add("flex-around");
+
+const topMenuLinks = topMenuEl.querySelectorAll('a');
+
+topMenuEl.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (event.target.tagName !== "A") return;
+
+
+  topMenuArray()
+  // remove active from all other links
+  topMenuLinks.forEach(link => {
+    if (link !== event.target) link.classList.remove("active");
+  });
+
+  // ✅ Toggle active FIRST
+  event.target.classList.toggle("active");
+
+  // clear out submenu each click
+  subMenu.innerHTML = "";
+
+  // cache clicked link object
+  const linkObj = menuLinks.find(link => link.text === event.target.textContent);
+
+  if (event.target.classList.contains("active") && linkObj.subLinks) {
+    // show + populate submenu
+    linkObj.subLinks.forEach(subLink => {
+      const a = document.createElement("a");
+      a.setAttribute("href", subLink.href);
+      a.textContent = subLink.text;
+      subMenu.appendChild(a);
+    });
+    subMenu.style.top = "100%";
+  } else {
+    // hide submenu
+    subMenu.style.top = "0";
+  }
+});
+
+ console.log(topMenuLinks)
